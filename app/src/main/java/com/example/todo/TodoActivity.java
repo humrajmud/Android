75ca5dc744c7a46caf231ab11471dfa7a181ead4
humraj <humrajmudhar@hotmail.com>
2018-10-15 13:35:17 +0100

    commented code

--- app/src/main/java/com/example/todo/TodoActivity.java
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -15,19 +15,27 @@ public class TodoActivity extends AppCompatActivity {
     private int mTodoIndex = 0;
 
     @Override
+
+    //class has one Activity method, onCreate(Bundle) which is called when an instance of the activity subclass is created.
     protected void onCreate(Bundle savedInstanceState) {
 
         // call the super class onCreate to complete the creation of activity like
         // the view hierarchy
+        // call the super class onCreate to complete the creation of Activity
+        // like the view hierarchy
         super.onCreate(savedInstanceState);
 
-        // set the user interface layout for this Activity
-        // the layout file is defined in the project res/layout/activity_todo.xml file
+        // UI, setContentView(R.layout.activity_todo) is called
+        //method inflates each widget in the layout XML resources file and instantiate it as defined by its attributes, hence the equivalent object.
+        //once id is defined can be referenced with a method findViewById(R.id.name)
         setContentView(R.layout.activity_todo);
 
         // initialize member TextView so we can manipulate it later
+        //once id is defined can be referenced with a method findViewById(R.id.name)
+        //Activity has a method findViewById(int id) which returns a View object for the widget id passed to it.
         final TextView TodoTextView;
         TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+
 
         // read the todo array from res/values/strings.xml
         Resources res = getResources();
@@ -39,6 +47,7 @@ public class TodoActivity extends AppCompatActivity {
         buttonNext = (Button) findViewById(R.id.buttonNext);
 
         // OnClick listener for the  Next button
+        //the onClick(View v) method is implemented as it is required by the OnClickListener interface.
         buttonNext.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
