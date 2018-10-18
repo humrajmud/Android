5f79e5d8dca64f77f37436d3046b756483493117
humraj <humrajmudhar@hotmail.com>
2018-10-18 12:42:16 +0100

    array problem fixed

--- app/src/main/java/com/example/todo/TodoActivity.java
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -103,7 +103,9 @@ public class TodoActivity extends AppCompatActivity {
         buttonNext.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
                 mTodoIndex += 1;
+                mTodoIndex = (mTodoIndex + 1) % mTodos.length;
                 TodoTextView.setText(mTodos[mTodoIndex]);
             }
         });
